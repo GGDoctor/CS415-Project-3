6038b93fdd8605062f6288ab0e8a445a26e6ee31
GGDoctor <jmfranco02@gmail.com>
2023-12-13 23:41:54 -0800

    Final Push

--- Main.cpp
+++ Main.cpp
@@ -9,6 +9,9 @@ struct Vertex{
     int x;
     int y;
 };
+
+//Checks to see if a pixel and a neighbor are actually neighbors
+// If we were to add a color check here for seeing if it is a valid pixel it produces an infinite loop or easily something of O(n^3) time
 bool isNeighbor(BMP Image, Vertex u, Vertex v){
     if(abs(u.x - v.x) + abs(u.y - v.y) == 1  && Image(u.x, u.y) != Image(v.x, v.y)){
         return true;
@@ -16,55 +19,57 @@ bool isNeighbor(BMP Image, Vertex u, Vertex v){
     return false;
 }
 
+//Breath first search function as described in project outline
+//Problem -> this will not output a correct BMP file for some reason. Leads to pixels breaking and ignoring paths
 void Breath_First_Search(BMP Image,Vertex start, Vertex end,string outputFile ){
     queue<Vertex> Q; //Creates our queue
-    vector<bool> visited(Image.TellWidth()*Image.TellHeight(), false);  //Creates our visited array
+    vector<bool> visited(Image.TellWidth() * Image.TellHeight(), false);  //Creates our visited array
     vector<int> d(Image.TellWidth() * Image.TellHeight(), INT_MAX); //Creates our distance array
-    vector<Vertex> prev(Image.TellHeight()*Image.TellWidth(),Vertex());
+    vector<Vertex> prev(Image.TellHeight() * Image.TellWidth(),Vertex());
 
     //Starting Point
     Q.push(start);
     visited[start.y * Image.TellWidth() + start.x] = true;
     d[start.y * Image.TellWidth() + start.x] = 0;
 
-    //BFS
+    //Breath First Search
     while(!Q.empty() && !visited[end.y * Image.TellWidth() + end.x]){
         Vertex u = Q.front();
         Q.pop();
 
         for (int dx = -1; dx < 1; ++dx){
             for (int dy = -1; dy < 1; ++dy){
-                if (dx == 0 && dy == 0) {
-                    continue;
-                }
-                Vertex v;
-                v.x = u.x + dx;
-                v.y = u.y +dy;
+                Vertex v = {u.x + dx,u.y + dy};
 
-                if (v.x >= 0 && v.x < Image.TellWidth() && v.y >= 0 && v.y < Image.TellHeight() && !visited[v.y * Image.TellWidth() +v.x] && isNeighbor(Image, u, v)){
+                if (v.x >= 0 && v.x < Image.TellWidth() && v.y >= 0 && v.y < Image.TellHeight() && isNeighbor(Image, u, v) && !visited[v.y * Image.TellWidth() + v.x]){
                     visited[v.y * Image.TellWidth() + v.x] = true;
-                    d[v.y * Image.TellWidth() + v.x] = d[u.y * Image.TellWidth() + u.x] +1;
-                    prev[v.y * Image.TellWidth() + v.x] = u;
                     Image(v.x, v.y)->Green = 255;
                     Image(v.x, v.y)->Blue = 0;
-                    Image(v.x,v.y)->Red =0;
+                    Image(v.x,v.y)->Red = 0;
+                    d[v.y * Image.TellWidth() + v.x] = d[u.y * Image.TellWidth() + u.x] +1;
+                    prev[v.y * Image.TellWidth() + v.x] = u;
                     Q.push(v);
                 }
             }
         }
     }
+
     // If target found, create new image and copy modified pixels
     if (!visited[end.y * Image.TellWidth() + end.x]) {
         BMP newImg; // Create an empty image with the same size
         newImg.SetSize(Image.TellWidth(), Image.TellHeight());
 
         Vertex v = end;
-        while (v.x != start.x && v.y != start.y) {
+        while (v.x != start.x || v.y != start.y) {
             newImg(v.x, v.y)->Red = 255;
             newImg(v.x, v.y)->Green = 0;
-            newImg(v.y, v.y)->Blue = 0;
+            newImg(v.x, v.y)->Blue = 0;
             v = prev[v.y * Image.TellWidth() + v.x];
         }
+        Image(start.x, start.y)->Green = 255;
+        Image(start.x, start.y)->Red = 0;
+        Image(start.x, start.y)->Blue = 0;
+
         for (int i = 0; i < Image.TellWidth(); i++) {
             for (int j = 0; j < Image.TellHeight(); j++) {
                 newImg(i, j)->Red = Image(i, j)->Red;
@@ -72,7 +77,6 @@ void Breath_First_Search(BMP Image,Vertex start, Vertex end,string outputFile ){
                 newImg(i, j)->Blue = Image(i, j)->Blue;
             }
         }
-
         // Write the pathfinding visualization to the specified file
         newImg.WriteToFile(outputFile.c_str());
     }
@@ -114,7 +118,7 @@ void Best_First_Search(BMP Image, Vertex start, Vertex end,string outputFile){
                     visited[v.y * Image.TellWidth() + v.x] = true;
                     Image(v.x, v.y)->Green = 255;
                     Image(v.x, v.y)->Blue = 0;
-                    Image(v.x,v.y)->Red =0;
+                    Image(v.x,v.y)->Red = 0;
                     d[v.y * Image.TellWidth() + v.x] = d[u.y * Image.TellWidth() + u.x] + 1;
                     prev[v.y * Image.TellWidth() + v.x] = u;
                     int h = abs(v.x - end.x) + abs(v.y - end.y); // Manhattan distance as heuristic
@@ -153,9 +157,14 @@ void Best_First_Search(BMP Image, Vertex start, Vertex end,string outputFile){
     }
 }
 
+//Prompts the user for a file to read
+//Prompts the user for a start and end vert
+//Prompts the user for the two files to create later on once the algorithms are run
+
+//Weird bug: when inputting vertex values leads to random spaces on following input prompts
 int main(){
     string imageFile;
-    cout<<"Enter the filename of your BMP image: "<< endl;
+    cout << "Enter the filename of your BMP image: "<< endl;
     cin >> imageFile;
 
     BMP image;
@@ -167,19 +176,22 @@ int main(){
     }
 
     Vertex start, end;
-    cout << "Enter the starting point coordinates (X,Y): ";
+    cout << "Notice the spaces in between the x and y value on the following prompt: 8 8" << endl;
+    cout << "Enter the starting point coordinates (X Y): " << endl;
     cin >> start.x >> start.y;
 
-    cout<< "Enter the ending point coordinates (X,Y): ";
+    cout<< "Enter the ending point coordinates (X Y): " << endl;
     cin >> end.x >> end.y;
 
-
     string bfsOutputfile, bestfirstOutputfile;
-    cout << "Enter the filename for the Breath First output image (e.g. bfs_output.bmp): ";
+
+    cout << "Enter the filename for the Breath First output image (e.g. bfs_output.bmp): " << endl;
     cin >> bfsOutputfile;
-    cout << "Enter the filename for the Best First output image (e.g. bfs_output.bmp): ";
+
+
+    cout << "Enter the filename for the Best First output image (e.g. bfs_output.bmp): " << endl;
     cin >> bestfirstOutputfile;
-    //cout<<"Broken here"<<endl;
+
     Breath_First_Search(image,start,end,bfsOutputfile);
     Best_First_Search(image, start,end,bestfirstOutputfile);
 
--- README.md
+++ README.md
@@ -1,5 +1,26 @@
 # CS415-Project-3
-This project takes as an input a BMP file and two vertexes in the for of x y.
 
-Follow the instructions given by the program. Some of the spacing may break as x y for each vertex creats a random
-newline character.
+#Overview
+This project takes as an input a BMP file and two vertexes in the for of x,y.
+
+This program will prompt for inputs. There is no need to enter your inputs through terminal. 
+
+Some things to note about this program. BFS does not work as intended. The code is written so it can perform a BFS search.
+However, the code does not execute properly when it comes to processing the pixels and outputs an incorrect BMP file with 
+only green on the 0,0 of the file.
+
+Best First Search does work. The only problem with this portion of the program is around the isNeighbor function. 
+The function does not check pixel values to see if they are within a certain thresh-hold to see if the object is a wall. 
+This creates issues and causes the program to go through walls. 
+
+The isNeighbor function had a pixel check at one point however, this lead to runtime issues that we were not able to solve. 
+This was an unintended consequence and had to be scrapped to stop the execution from going on too long.
+
+Lastly, the files do output. They are hidden under the cmake-build-debug folder under the name that you inputted. 
+You are welcome to delete old files if they are still there from previous tests. 
+
+#Errors
+***
+If there are any issues compiling the code check the CMake to make sure all the files are linked correctly. There was a linker
+error in the code earlier when running it in VS code as VS Code would not generate a CMake file. 
+****
